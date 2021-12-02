8c09741b799a9bbd846f1034d5774b8ace92b6c5
yngtdd <yngtdd@gmail.com>
2021-12-01 23:43:10 -0500

    Rename data.txt
    Tab completion was getting tedious

--- day01/day01.ml
+++ day01/day01.ml
@@ -1,4 +1,10 @@
 open Core
 
-let file = "data.txt"
-let r = In_channel.read_lines file
+let file = "input.txt"
+
+let convert (input: string list) : int list = 
+    List.map input ~f:Int.of_string 
+
+let () = 
+    let input = In_channel.read_lines file |> List.map ~f:String.strip in
+    input |> List.map convert |> Printf.printf "part 1: %i\n";

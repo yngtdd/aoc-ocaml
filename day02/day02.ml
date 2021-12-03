7835ee2aef96a74f6251bc17f78be683d2791103
yngtdd <yngtdd@gmail.com>
2021-12-03 12:24:37 -0500

    Add regex for parsing input into coord type

--- day02/day02.ml
+++ day02/day02.ml
@@ -1,9 +1,21 @@
 open Core
+open Re
 
 type coordinate = 
     { direction : string; 
       magnitude : int; 
     }
 
+let coordinate_of_string line = 
+  let matches =
+    let pat = "([a-zA-Z]+)[ \t]+([0-9])" in 
+    Re.exec (Re.Posix.compile_pat pat) line 
+  in 
+  { direction = Re.Group.get matches 1;
+    magnitude = Int.of_string (Re.Group.get matches 2);
+  };;
+
+
 let input = 
     In_channel.read_lines "input.txt"
+    |> List.map ~f:coordinate_of_string

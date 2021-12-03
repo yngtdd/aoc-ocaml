346cd2fbf4d8c90fc9df1806828a95f5e4ccd41f
yngtdd <yngtdd@gmail.com>
2021-12-03 14:43:41 -0500

    Add ocamlformat

--- src/day01/day01.ml
+++ src/day01/day01.ml
@@ -1,25 +1,26 @@
 open Core
 open Printf
 
-let input = 
-  In_channel.read_lines "input.txt"
-  |> List.map ~f:Int.of_string
+let input = In_channel.read_lines "input.txt" |> List.map ~f:Int.of_string
 
 let result a b = if a < b then 1 else 0
 
 let rec increasing acc data =
   match data with
   | [] -> acc
-  | [_] -> acc
-  | first :: second :: rest -> increasing (acc + result first second)
-                               (second :: rest);;
+  | [ _ ] -> acc
+  | first :: second :: rest ->
+      increasing (acc + result first second) (second :: rest)
 
-let rec three_increasing acc data = 
-  match data with 
+let rec three_increasing acc data =
+  match data with
   | [] -> acc
-  | [_] -> acc
-  | a :: b :: c :: d :: rest -> three_increasing (acc + (result (a+b+c) (b+c+d))) 
-                                (b :: c :: d :: rest)
-  | _ -> acc;;
+  | [ _ ] -> acc
+  | a :: b :: c :: d :: rest ->
+      three_increasing
+        (acc + result (a + b + c) (b + c + d))
+        (b :: c :: d :: rest)
+  | _ -> acc
+;;
 
 printf "%d %d\n" (increasing 0 input) (three_increasing 0 input)
--- src/day02/day02.ml
+++ src/day02/day02.ml
@@ -1,25 +1,22 @@
 open Core
 
+type coordinate = { direction : string; magnitude : int }
 (** [coordinate] of submarine's movement
 
     At each step in the submarine's path, 
     we move in a direction [up, down, left, right]
     with a given magnitude. *)
-type coordinate = 
-  { direction : string; 
-    magnitude : int; 
+
+(** Construct a [coordinate] from a parsed string line *)
+let coordinate_of_string line =
+  let matches =
+    let pat = "([a-zA-Z]+)[ \t]+([0-9])" in
+    Re.exec (Re.Posix.compile_pat pat) line
+  in
+  {
+    direction = Re.Group.get matches 1;
+    magnitude = Int.of_string (Re.Group.get matches 2);
   }
 
-  (** Construct a [coordinate] from a parsed string line *)
-let coordinate_of_string line = 
-  let matches =
-    let pat = "([a-zA-Z]+)[ \t]+([0-9])" in 
-    Re.exec (Re.Posix.compile_pat pat) line 
-  in 
-  { direction = Re.Group.get matches 1;
-    magnitude = Int.of_string (Re.Group.get matches 2);
-  };;
-
-let input = 
-  In_channel.read_lines "input.txt"
-  |> List.map ~f:coordinate_of_string
+let input =
+  In_channel.read_lines "input.txt" |> List.map ~f:coordinate_of_string

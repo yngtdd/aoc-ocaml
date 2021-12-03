9d5854b38a304756a242134729eb3ced2caacd14
yngtdd <yngtdd@gmail.com>
2021-12-03 01:24:23 -0500

    Add pattern matching solution

--- day01/day01.ml
+++ day01/day01.ml
@@ -1,6 +1,17 @@
 open Core
+open Printf
 
 let input = 
     In_channel.read_lines "input.txt"
     |> List.map ~f:Int.of_string
 
+let result a b = if a < b then 1 else 0
+
+let rec increasing acc data =
+  match data with
+  | [] -> acc
+  | [_] -> acc
+  | first :: second :: rest -> increasing (acc + result first second)
+                                 (second :: rest);;
+
+printf "%d\n" (increasing 0 input)

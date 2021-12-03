528c752371b4ffc27224c589781fd5f9db56cb64
yngtdd <yngtdd@gmail.com>
2021-12-03 01:07:12 -0500

    Fix conversion from string -> int

--- day01/day01.ml
+++ day01/day01.ml
@@ -1,10 +1,6 @@
 open Core
 
-let file = "input.txt"
+let input = 
+    In_channel.read_lines "input.txt"
+    |> List.map ~f:Int.of_string
 
-let convert (input: string list) : int list = 
-    List.map input ~f:Int.of_string 
-
-let () = 
-    let input = In_channel.read_lines file |> List.map ~f:String.strip in
-    input |> List.map convert |> Printf.printf "part 1: %i\n";
